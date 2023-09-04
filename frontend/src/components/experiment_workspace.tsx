90593f6056627bf90ea09db8d6efeeebb58d6fc1
Jinyu Xie <xjygr08@gmail.com>
2023-09-03 23:17:17 -0700

    dynamic grid columns based on screen size

--- frontend/src/components/experiment_workspace.tsx
+++ frontend/src/components/experiment_workspace.tsx
@@ -14,7 +14,7 @@ export default function ExperimentWorkspace({
   return (
     <div className="">
       {exp ? (
-        <div className="w-full max-auto grid grid-col-2">
+        <div className="w-full max-auto grid-col-1 xl:grid-col-2">
           <div className="p-2 w-full">
             <div className="card shadow bg-base-100">
               <div className="card-body">
@@ -25,13 +25,13 @@ export default function ExperimentWorkspace({
               </div>
             </div>
           </div>
-          <div className="col-start-2 p-2">
+          <div className="xl:col-start-2 p-2">
             <MetricCard exps={compExps} />
           </div>
-          <div className="p-2 col-span-2 w-full">
+          <div className="p-2 xl:col-span-2 w-full">
             <SimulationReplayer exps={compExps} />
           </div>
-          <div className="p-2 col-span-2 w-full">
+          <div className="p-2 xl:col-span-2 w-full">
             <VariableMonitor env_id={exp.environment_id} exps={compExps} />
           </div>
         </div>
