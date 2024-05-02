63e052fc4d35f8d5030b7e657f00ff5dddcfa9dd
Jinyu Xie <xjygr08@gmail.com>
2024-05-01 21:12:52 -0700

    Simplify the dependencies

--- setup.py
+++ setup.py
@@ -1,29 +1,34 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
-
-def parse_requirements(filename):
-    """Load requirements from a pip requirements file."""
-    with open(filename) as f:
-        lineiter = (line.strip() for line in f)
-        return [line for line in lineiter if line and not line.startswith("#")]
-
-
-reqs = parse_requirements("requirements.txt")
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
     name="loopquest",
-    version="0.2.0",
+    version="0.2.1",
     description="A Production Tool for Embodied AI.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     author="LoopMind",
     author_email="contactus@loopmind.ai",
     url="https://github.com/LoopMind-AI/loopquest",
-    packages=["loopquest"],
-    install_requires=reqs,
+    packages=find_packages(),
+    install_requires=[
+        "gymnasium>=0.29.0",
+        "huggingface-hub>=0.16.4",
+        "datasets>=2.14.4",
+        "python-dotenv>=1.0.0",
+        "stable-baselines3>=2.2.1",
+        "huggingface-sb3>=3.0",
+        "pydantic>=2.1.1",
+    ],
+    extras_require={
+        "dev": ["pytest", "twine"],
+        "rl": ["ale_py>=0.8", "box2d-py==2.3.5", "pygame>=2.1.3", "swig==4.*"],
+        "mujoco-py": ["mujoco-py>=2.1,<2.2", "cython<3"],
+        "mujoco": ["mujoco>=2.1.5", "imageio>=2.14.1"],
+    },
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Intended Audience :: Developers",
